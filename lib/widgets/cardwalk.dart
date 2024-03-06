fe9f208b465ff9a6c1880474582e91d4559e7464
KENTHN658 <118724637+KENTHN658@users.noreply.github.com>
2024-03-07 00:27:29 +0700

    last of day

--- lib/widgets/cardwalk.dart
+++ lib/widgets/cardwalk.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 
 import '../provider/walk_provider.dart';
 
+
 class CardWalk extends StatefulWidget {
   const CardWalk({Key? key}) : super(key: key);
 
@@ -12,21 +13,26 @@ class CardWalk extends StatefulWidget {
 }
 
 class _CardWalkState extends State<CardWalk> {
-
   void _incrementCounter(CoinDisplayProvider coin) {
     setState(() {
       coin.incrementScorePoints(10);
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    final walkProvider = Provider.of<WalkProvider>(context);
-    if ((int.parse(walkProvider.steps)) == 7000) {
-      _incrementCounter;
+    var walkProvider = Provider.of<WalkProvider>(context);
+    int steps = 0;
+    if (walkProvider.steps != null) {
+      steps = int.tryParse(walkProvider.steps!) ?? 0;
+      if (steps == 7000) {
+        _incrementCounter(context.read<CoinDisplayProvider>());
+      }
     }
+
     return Card(
       elevation: 5,
-        margin: EdgeInsets.all(20),
+      margin: EdgeInsets.all(20),
       child: Padding(
         padding: EdgeInsets.all(10),
         child: Column(
@@ -36,84 +42,85 @@ class _CardWalkState extends State<CardWalk> {
               children: [
                 Text(
                   "การเดิน",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black), // Adjusted text color
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Row(
               children: [
                 Text(
                   "วันนี้",
-                  style: TextStyle(fontSize: 16, color: Colors.black), // Adjusted text color
+                  style: TextStyle(fontSize: 16, color: Colors.black),
                 ),
-                SizedBox(
-                  width: 10,
-                ),
+                SizedBox(width: 10),
                 Text(
-                  '${walkProvider.steps} ก้าว' ,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black), // Adjusted text color
+                  '${walkProvider.steps ?? '0'} ก้าว',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Row(
               children: [
                 Text(
                   "ระยะทาง",
-                  style: TextStyle(fontSize: 16, color: Colors.black), // Adjusted text color
+                  style: TextStyle(fontSize: 16, color: Colors.black),
                 ),
-                SizedBox(
-                  width: 10,
-                ),
+                SizedBox(width: 10),
                 Text(
-                  "${(int.parse(walkProvider.steps) * 0.762 / 1000).toStringAsFixed(2)} กม.",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+                  "${(steps * 0.762 / 1000).toStringAsFixed(2)} กม.",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
             Row(
               children: [
                 Text(
                   "แคลอรี่ที่เผาผลาญ",
-                  style: TextStyle(fontSize: 16, color: Colors.black), // Adjusted text color
+                  style: TextStyle(fontSize: 16, color: Colors.black),
                 ),
-                SizedBox(
-                  width: 10,
-                ),
+                SizedBox(width: 10),
                 Text(
-                  "${(int.parse(walkProvider.steps) * 0.762 / 1000 *30).toStringAsFixed(2)} แคลอรี่",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+                  "${(steps * 0.762 / 1000 * 30).toStringAsFixed(2)} แคลอรี่",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Divider(
-              color: Colors.grey,
-            ),
-            SizedBox(
-              height: 10,
-            ),
+            SizedBox(height: 10),
+            Divider(color: Colors.grey),
+            SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
                   "เป้าหมายการเดิน",
-                  style: TextStyle(fontSize: 16, color: Colors.black), // Adjusted text color
+                  style: TextStyle(fontSize: 16, color: Colors.black),
                 ),
                 Text(
                   "7,000 ก้าว",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black), // Adjusted text color
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
               ],
             ),
